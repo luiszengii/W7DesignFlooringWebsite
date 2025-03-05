39ed44c2252542f5e77f6b623d02dab169e920a3
Luis Zeng <lingjun.zeng@stepglobal.com>
2025-03-05 16:02:40 +1100

    update footer

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -5,13 +5,8 @@ import { Typography } from "@mui/material";
 
 export default function Copyright() {
   return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      sx={{ mt: 4 }}
-    >
-      © {new Date().getFullYear()} Premium Floors
+    <Typography variant="body2" color="text.secondary" align="center" sx={{ mt: 4 }}>
+      © {new Date().getFullYear()} W7 Design Flooring
     </Typography>
   );
 }
